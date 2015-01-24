66efb6588502745ceabe404b3de5982f8a23192e
Andy Holt <andrew.holt@hotmail.co.uk>
2015-01-24 17:59:07 +0000

    Basic Ledger functionality all working.

--- src/ledger.py
+++ src/ledger.py
@@ -194,3 +194,287 @@ class Ledger:
         self.close_cursor()
 
         return transactions_list
+
+    def add_transaction(self, date, payee_id, description, amount):
+        """
+        Add a transaction to the database with the specified values.
+
+        Arguments:
+        - `date`: A datetime.date object consisting of the date.
+        - `payee_id`: The payee_id of the payee.
+        - `description`: String describing the transaction.
+        - `amount`: Amount of transaction
+        """
+        # [todo] - implement error handling and parameter checking pre-execution
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        self.reset_auto_increment('transactions')
+
+        # add transaction with required values
+        stmt = "INSERT INTO transactions " + \
+               "VALUES ('0', " + \
+               "'{0}-{1}-{2}', ".format(date.year, date.month, date.day) + \
+               "'{0}', '{1}', ".format(payee_id, description) + \
+               "'{0}')".format(amount)
+
+        cur.execute(stmt)
+
+        # close the cursor
+        self.close_cursor()
+
+    def edit_transaction(self, trans_id, date, payee_id, description, amount):
+        """
+        Edit transaction trans_id to take on new values.
+
+        Arguments:
+        - `trans_id`: transasction to be modified
+        - `date`: new date
+        - `payee_id`: new payee id
+        - `description`: new description
+        - `amount`: new amount
+        """
+        # [todo] - all parameters except trans_id optional, fill others with
+        # current values
+
+        # [todo] - validate transaction_id
+        # [todo] - validate new values
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        edit_trans_statement = "UPDATE transactions " + \
+                               "SET date='{0}-{1}-{2}', ".format(date.year,
+                                                                 date.month,
+                                                                 date.day) + \
+                               "payee_id='{0}', ".format(payee_id) + \
+                               "description='{0}', ".format(description) + \
+                               "amount='{0}' ".format(amount) + \
+                               "WHERE transaction_id={0}".format(trans_id)
+
+        cur.execute(edit_trans_statement)
+
+        # close the cursor
+        self.close_cursor()
+
+    def delete_transaction(self, trans_id):
+        """
+        Delete transaction with id trans_id
+
+        Arguments:
+        - `trans_id`: id of transaction to be deleted.
+        """
+        # [todo] - ensure no transaction_items depend on transaction to be
+        # deleted
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        delete_trans_statement = "DELETE FROM transactions " + \
+                                 "WHERE transaction_id={0}".format(trans_id)
+
+        cur.execute(delete_trans_statement)
+
+        # close the cursor
+        self.close_cursor()
+
+    def get_items(self):
+        """
+        Return a list of dicts of all transaction items.
+        """
+        # open a cursor object
+        cur = self.get_cursor()
+
+        # get payees from database
+        cur.execute("SELECT * FROM transaction_items")
+        items_data = cur.fetchall()
+
+        # convert into a list of payee dictionaries
+        items_list = []
+        [items_list.append({'transaction_item_id': item[0],
+                            'transaction_id': item[1],
+                            'description': item[2],
+                            'amount': item[3],
+                            'category_id': item[4]})
+         for item in items_data]
+
+        # close the cursor
+        self.close_cursor()
+
+        return items_list
+
+    def add_item(self, transaction_id, description, amount, category_id):
+        """
+        Add a transaction item to the database with the specified values.
+
+        Arguments:
+        - `transaction_id`: transaction to be associated to
+        - `description`: String describing the transaction.
+        - `amount`: Amount of transaction
+        - `category_id` category that item belongs to
+        """
+        # [todo] - implement error handling and parameter checking pre-execution
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        self.reset_auto_increment('transaction_items')
+
+        # add transaction with required values
+        stmt = "INSERT INTO transaction_items " + \
+               "VALUES ('0', " + \
+               "'{0}', '{1}', ".format(transaction_id, description) + \
+               "'{0}', '{1}')".format(amount, category_id)
+
+        cur.execute(stmt)
+
+        # close the cursor
+        self.close_cursor()
+
+    def edit_item(self, item_id, transaction_id, description, amount,
+                  category_id):
+        """
+        Edit transaction item item_id to take on new values.
+
+        Arguments:
+        - `item_id`: item to be modified
+        - `transaction_id`: new transaction to be associated with
+        - `description`: new description of item
+        - `amount`: new amount of item
+        - `category_id`: new category assignment
+        """
+        # [todo] - all parameters except item_id optional, fill others with
+        # current values
+
+        # [todo] - validate item_id
+        # [todo] - validate new values
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        stmt = "UPDATE transaction_items " + \
+               "SET transaction_id='{0}', ".format(transaction_id) + \
+               "description='{0}', ".format(description) + \
+               "amount='{0}', ".format(amount) + \
+               "category_id='{0}' ".format(category_id) + \
+               "WHERE transaction_item_id={0}".format(item_id)
+
+        cur.execute(stmt)
+
+        # close the cursor
+        self.close_cursor()
+
+    def delete_item(self, item_id):
+        """
+        Delete transaction item with id item_id
+
+        Arguments:
+        - `item_id`: id of transaction item to be deleted.
+        """
+        # open a cursor
+        cur = self.get_cursor()
+
+        delete_item_statement = "DELETE FROM transaction_items " + \
+                                "WHERE transaction_item_id={0}".format(item_id)
+
+        cur.execute(delete_item_statement)
+
+        # close the cursor
+        self.close_cursor()
+
+    def get_categories(self):
+        """
+        Return a list of dicts of all categories.
+        """
+        # open a cursor object
+        cur = self.get_cursor()
+
+        # get payees from database
+        cur.execute("SELECT * FROM categories")
+        cats_data = cur.fetchall()
+
+        # convert into a list of payee dictionaries
+        cats_list = []
+        [cats_list.append({'category_id': cat[0],
+                           'parent_id': cat[1],
+                           'category_name': cat[2]})
+         for cat in cats_data]
+
+        # close the cursor
+        self.close_cursor()
+
+        return cats_list
+
+    def add_category(self, category_name, parent_id):
+        """
+        Add a category to the database with the specified name and parent.
+
+        Arguments:
+        - `category_name`: category name
+        - `parent_id`: id of parent category to which this is a direct child
+        """
+        # [todo] - implement error handling and parameter checking pre-execution
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        self.reset_auto_increment('categories')
+
+        # add transaction with required values
+        stmt = "INSERT INTO categories " + \
+               "VALUES ('0', " + \
+               "'{0}', '{1}')".format(parent_id, category_name)
+
+        cur.execute(stmt)
+
+        # close the cursor
+        self.close_cursor()
+
+    def edit_category(self, category_id, category_name, parent_id):
+        """
+        Edit category category_id to take on new values.
+
+        Arguments:
+        - `category_id`: category to be edited
+        - `category_name`: new name of category
+        - `parent_id`: new parent for category
+        """
+        # [todo] - all parameters except category_id optional, fill others with
+        # current values
+
+        # [todo] - validate category_id
+        # [todo] - validate new values
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        stmt = "UPDATE categories " + \
+               "SET parent_id='{0}', ".format(parent_id) + \
+               "category_name='{0}' ".format(category_name) + \
+               "WHERE category_id={0}".format(category_id)
+
+        cur.execute(stmt)
+
+        # close the cursor
+        self.close_cursor()
+
+    def delete_category(self, category_id):
+        """
+        Delete category with id category_id
+
+        Arguments:
+        - `category_id`: id of category to be deleted.
+        """
+        # [todo] - add error handling
+
+        # open a cursor
+        cur = self.get_cursor()
+
+        delete_cat_statement = "DELETE FROM categories " + \
+                               "WHERE category_id={0}".format(category_id)
+
+        cur.execute(delete_cat_statement)
+
+        # close the cursor
+        self.close_cursor()
--- src/tests/ledger_tests.py
+++ src/tests/ledger_tests.py
@@ -168,3 +168,340 @@ class LedgerTests(unittest.TestCase):
         actual_result = self.ledger.get_transactions()
 
         self.assertEqual(actual_result, expected_result)
+
+    def test_add_transaction(self):
+        """
+        Ensure that transactions are correctly added to database
+        """
+        self.ledger.add_transaction(datetime.date(2015, 1, 3),
+                                    2,
+                                    'More shopping',
+                                    5.42)
+
+        expected_result = [{'date': datetime.date(2015, 1, 2),
+                            'amount': Decimal('46.85'),
+                            'description': 'Train ticket',
+                            'transaction_id': 1,
+                            'payee_id': 1},
+                           {'date': datetime.date(2015, 1, 3),
+                            'amount': Decimal('5.42'),
+                            'description': 'Shopping',
+                            'transaction_id': 2,
+                            'payee_id': 2},
+                           {'date': datetime.date(2015, 1, 3),
+                            'amount': Decimal('5.42'),
+                            'description': 'More shopping',
+                            'transaction_id': 3,
+                            'payee_id': 2}]
+        actual_result = self.ledger.get_transactions()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_edit_transaction(self):
+        """
+        Test modification of a transaction using edit_transaction_method.
+        """
+        self.ledger.edit_transaction(2,
+                                     datetime.date(2015,1,5),
+                                     1,
+                                     'Another train ticket',
+                                     42.00)
+
+        expected_result = [{'date': datetime.date(2015, 1, 2),
+                            'amount': Decimal('46.85'),
+                            'description': 'Train ticket',
+                            'transaction_id': 1,
+                            'payee_id': 1},
+                           {'date': datetime.date(2015, 1, 5),
+                            'amount': Decimal('42.00'),
+                            'description': 'Another train ticket',
+                            'transaction_id': 2,
+                            'payee_id': 1}]
+        actual_result = self.ledger.get_transactions()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_delete_transaction(self):
+        """
+        Test deletion of a transaction using delete_transaction method.
+        """
+        # first, create a transaction with no dependents
+        self.ledger.add_transaction(datetime.date(2015, 1, 3),
+                                    2,
+                                    'More shopping',
+                                    5.42)
+        expected_result = [{'date': datetime.date(2015, 1, 2),
+                            'amount': Decimal('46.85'),
+                            'description': 'Train ticket',
+                            'transaction_id': 1,
+                            'payee_id': 1},
+                           {'date': datetime.date(2015, 1, 3),
+                            'amount': Decimal('5.42'),
+                            'description': 'Shopping',
+                            'transaction_id': 2,
+                            'payee_id': 2},
+                           {'date': datetime.date(2015, 1, 3),
+                            'amount': Decimal('5.42'),
+                            'description': 'More shopping',
+                            'transaction_id': 3,
+                            'payee_id': 2}]
+        actual_result = self.ledger.get_transactions()
+        self.assertEqual(actual_result, expected_result)
+
+        # then delete the extra transaction
+        self.ledger.delete_transaction(3)
+
+        actual_result = self.ledger.get_transactions()
+        expected_result = [{'date': datetime.date(2015, 1, 2),
+                            'amount': Decimal('46.85'),
+                            'description': 'Train ticket',
+                            'transaction_id': 1,
+                            'payee_id': 1},
+                           {'date': datetime.date(2015, 1, 3),
+                            'amount': Decimal('5.42'),
+                            'description': 'Shopping',
+                            'transaction_id': 2,
+                            'payee_id': 2}]
+        self.assertEqual(actual_result, expected_result)
+
+    def test_get_items(self):
+        """
+        Ensure transaction items are correctly recieved from database
+        """
+        expected_result = [{'transaction_item_id': 1,
+                            'transaction_id': 1,
+                            'description': 'Train ticket: Aberdeen to Cambridge',
+                            'amount': Decimal('46.85'),
+                            'category_id': 3},
+                           {'transaction_item_id': 2,
+                            'transaction_id': 2,
+                            'description': 'Apples',
+                            'amount': Decimal('3.00'),
+                            'category_id': 4},
+                           {'transaction_item_id': 3,
+                            'transaction_id': 2,
+                            'description': 'Milk',
+                            'amount': Decimal('1.00'),
+                            'category_id': 5},
+                           {'transaction_item_id': 4,
+                            'transaction_id': 2,
+                            'description': 'Bread',
+                            'amount': Decimal('1.42'),
+                            'category_id': 5}]
+        actual_result = self.ledger.get_items()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_add_item(self):
+        """
+        Ensure that transaction items are correctly added to database
+        """
+        self.ledger.add_item(2, 'Bananas', 0.67, 4)
+
+        expected_result = [{'transaction_item_id': 1,
+                            'transaction_id': 1,
+                            'description': 'Train ticket: Aberdeen to Cambridge',
+                            'amount': Decimal('46.85'),
+                            'category_id': 3},
+                           {'transaction_item_id': 2,
+                            'transaction_id': 2,
+                            'description': 'Apples',
+                            'amount': Decimal('3.00'),
+                            'category_id': 4},
+                           {'transaction_item_id': 3,
+                            'transaction_id': 2,
+                            'description': 'Milk',
+                            'amount': Decimal('1.00'),
+                            'category_id': 5},
+                           {'transaction_item_id': 4,
+                            'transaction_id': 2,
+                            'description': 'Bread',
+                            'amount': Decimal('1.42'),
+                            'category_id': 5},
+                           {'transaction_item_id': 5,
+                            'transaction_id': 2,
+                            'description': 'Bananas',
+                            'amount': Decimal('0.67'),
+                            'category_id': 4}]
+        actual_result = self.ledger.get_items()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_edit_item(self):
+        """
+        Test modification of a transaction item using edit_item method.
+        """
+        self.ledger.edit_item(4, 1, 'More train tickets', 25.34, 3)
+
+        expected_result = [{'transaction_item_id': 1,
+                            'transaction_id': 1,
+                            'description': 'Train ticket: Aberdeen to Cambridge',
+                            'amount': Decimal('46.85'),
+                            'category_id': 3},
+                           {'transaction_item_id': 2,
+                            'transaction_id': 2,
+                            'description': 'Apples',
+                            'amount': Decimal('3.00'),
+                            'category_id': 4},
+                           {'transaction_item_id': 3,
+                            'transaction_id': 2,
+                            'description': 'Milk',
+                            'amount': Decimal('1.00'),
+                            'category_id': 5},
+                           {'transaction_item_id': 4,
+                            'transaction_id': 1,
+                            'description': 'More train tickets',
+                            'amount': Decimal('25.34'),
+                            'category_id': 3}]
+        actual_result = self.ledger.get_items()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_delete_item(self):
+        """
+        Test deletion of a transaction item using delete_item method.
+        """
+        self.ledger.delete_item(3)
+
+        expected_result = [{'transaction_item_id': 1,
+                            'transaction_id': 1,
+                            'description': 'Train ticket: Aberdeen to Cambridge',
+                            'amount': Decimal('46.85'),
+                            'category_id': 3},
+                           {'transaction_item_id': 2,
+                            'transaction_id': 2,
+                            'description': 'Apples',
+                            'amount': Decimal('3.00'),
+                            'category_id': 4},
+                           {'transaction_item_id': 4,
+                            'transaction_id': 2,
+                            'description': 'Bread',
+                            'amount': Decimal('1.42'),
+                            'category_id': 5}]
+        actual_result = self.ledger.get_items()
+        self.assertEqual(actual_result, expected_result)
+
+    def test_get_categories(self):
+        """
+        Ensure categories are correctly recieved from database
+        """
+        expected_result = [{'category_id': 1,
+                            'parent_id': None,
+                            'category_name': 'travel'},
+                           {'category_id': 2,
+                            'parent_id': None,
+                            'category_name': 'food'},
+                           {'category_id': 3,
+                            'parent_id': 1,
+                            'category_name': 'train'},
+                           {'category_id': 4,
+                            'parent_id': 2,
+                            'category_name': 'fruit'},
+                           {'category_id': 5,
+                            'parent_id': 2,
+                            'category_name': 'staple'}]
+        actual_result = self.ledger.get_categories()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_add_category(self):
+        """
+        Ensure that categories are correctly added to database
+        """
+        self.ledger.add_category('coach', 1)
+
+        expected_result = [{'category_id': 1,
+                            'parent_id': None,
+                            'category_name': 'travel'},
+                           {'category_id': 2,
+                            'parent_id': None,
+                            'category_name': 'food'},
+                           {'category_id': 3,
+                            'parent_id': 1,
+                            'category_name': 'train'},
+                           {'category_id': 4,
+                            'parent_id': 2,
+                            'category_name': 'fruit'},
+                           {'category_id': 5,
+                            'parent_id': 2,
+                            'category_name': 'staple'},
+                           {'category_id': 6,
+                            'parent_id': 1,
+                            'category_name':'coach'}]
+        actual_result = self.ledger.get_categories()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_edit_category(self):
+        """
+        Test modification of a category using edit_category method.
+        """
+        self.ledger.edit_category(4, 'coach', 1)
+
+        expected_result = [{'category_id': 1,
+                            'parent_id': None,
+                            'category_name': 'travel'},
+                           {'category_id': 2,
+                            'parent_id': None,
+                            'category_name': 'food'},
+                           {'category_id': 3,
+                            'parent_id': 1,
+                            'category_name': 'train'},
+                           {'category_id': 4,
+                            'parent_id': 1,
+                            'category_name': 'coach'},
+                           {'category_id': 5,
+                            'parent_id': 2,
+                            'category_name': 'staple'}]
+        actual_result = self.ledger.get_categories()
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_delete_category(self):
+        """
+        Test deletion of a cateory using delete_category method.
+        """
+        # first, create a category with no dependents
+        self.ledger.add_category('coach', 1)
+        expected_result = [{'category_id': 1,
+                            'parent_id': None,
+                            'category_name': 'travel'},
+                           {'category_id': 2,
+                            'parent_id': None,
+                            'category_name': 'food'},
+                           {'category_id': 3,
+                            'parent_id': 1,
+                            'category_name': 'train'},
+                           {'category_id': 4,
+                            'parent_id': 2,
+                            'category_name': 'fruit'},
+                           {'category_id': 5,
+                            'parent_id': 2,
+                            'category_name': 'staple'},
+                           {'category_id': 6,
+                            'parent_id': 1,
+                            'category_name':'coach'}]
+        actual_result = self.ledger.get_categories()
+        self.assertEqual(actual_result, expected_result)
+
+        # then delete the extra transaction
+        self.ledger.delete_category(6)
+
+        expected_result = [{'category_id': 1,
+                            'parent_id': None,
+                            'category_name': 'travel'},
+                           {'category_id': 2,
+                            'parent_id': None,
+                            'category_name': 'food'},
+                           {'category_id': 3,
+                            'parent_id': 1,
+                            'category_name': 'train'},
+                           {'category_id': 4,
+                            'parent_id': 2,
+                            'category_name': 'fruit'},
+                           {'category_id': 5,
+                            'parent_id': 2,
+                            'category_name': 'staple'}]
+        actual_result = self.ledger.get_categories()
+
+        self.assertEqual(actual_result, expected_result)
